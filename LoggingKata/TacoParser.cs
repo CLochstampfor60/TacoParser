aa0897703ac30eba92607d82ae772e9db88ba6e3
CLochstampfor60 <cflochjr@gmail.com>
2024-01-10 15:38:12 -0500

    next update

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,9 +1,11 @@
-﻿namespace LoggingKata
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LoggingKata
 {
     /// <summary>
     /// Parses a POI file to locate all the Taco Bells
     /// </summary>
-    public class TacoParser
+    public class TacoParser 
     {
         readonly ILog logger = new TacoLogger();
         
@@ -24,19 +26,20 @@
             // TODO: Grab the latitude from your array at index 0
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            var latitude  = double.Parse(cells[0]);    
+
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
+            var longitude = double.Parse(cells[1]);
             
             // TODO: Grab the name from your array at index 2
-            
+            var name = double.Parse(cells[2]);
 
             // TODO: Create a TacoBell class
             // that conforms to ITrackable
             
+
             // TODO: Create an instance of the Point Struct
             // TODO: Set the values of the point correctly (Latitude and Longitude) 
 
